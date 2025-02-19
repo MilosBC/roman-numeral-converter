1968d6c6b881eaa587cc2ab02067b6303df766ef
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2025-02-19 01:08:29 +0100

    Add four-digit numbers
    The converter can now work with four-digit numbers (up to 4000)

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const numberInput = document.querySelector('#number');
 const outputText = document.querySelector('#output');
 const convertButton = document.querySelector('#convert-btn');
-//let result = '';
 
 function removeHidden() {
    if (outputText.classList.contains('hidden')) {
@@ -17,7 +16,7 @@ function displayRomanNumber(num) {
 
        if (textNum.length === 1) {
        switch(textNum) {
-        case '1':
+       case '1':
             result += 'I';
             break;
 
@@ -258,6 +257,140 @@ function displayRomanNumber(num) {
                 break;
                  
            }
+      } else if (textNum.length === 4) {
+         splitValues = textNum.split('');
+
+         switch(splitValues[0]) {
+            case '1':
+                result += 'M';
+                break;
+    
+             case '2':
+                result += 'MM';
+                break;
+    
+             case '3':
+                result += 'MMM';
+                break;
+         }
+
+         switch(splitValues[1]) {
+            case '1':
+                result += 'C';
+                break;
+    
+             case '2':
+                result += 'CC';
+                break;
+    
+             case '3':
+                result += 'CCC';
+                break;
+    
+             case '4':
+                result += 'CD';
+                break;
+    
+             case '5':
+                result += 'D';
+                break;
+             case '6':
+                result += 'DC';
+                break;
+    
+             case '7':
+                result += 'DCC';
+                break;
+    
+             case '8':
+                result += 'DCCC';
+                break;
+    
+             case '9':
+                result += 'CM';
+                break;
+                 
+           }
+
+           switch(splitValues[2]) {
+            case '1':
+                result += 'X';
+                break;
+    
+             case '2':
+                result += 'XX';
+                break;
+    
+             case '3':
+                result += 'XXX';
+                break;
+    
+             case '4':
+                result += 'XL';
+                break;
+    
+             case '5':
+                result += 'L';
+                break;
+             case '6':
+                result += 'LX';
+                break;
+    
+             case '7':
+                result += 'LXX';
+                break;
+    
+             case '8':
+                result += 'LXXX';
+                break;
+    
+             case '9':
+                result += 'XC';
+                break;
+                 
+           }
+
+           switch(splitValues[3]) {
+            case '0':
+               result += '';
+               break;
+   
+            case '1':
+                result += 'I';
+                break;
+    
+             case '2':
+                result += 'II';
+                break;
+    
+             case '3':
+                result += 'III';
+                break;
+    
+             case '4':
+                result += 'IV';
+                break;
+    
+             case '5':
+                result += 'V';
+                break;
+             case '6':
+                result += 'VI';
+                break;
+    
+             case '7':
+                result += 'VII';
+                break;
+    
+             case '8':
+                result += 'VIII';
+                break;
+    
+             case '9':
+                result += 'IX';
+                break;
+                 
+           }
       }
 
        
@@ -272,15 +405,8 @@ function displayRomanNumber(num) {
 
 convertButton.addEventListener('click', ()=> {
 
-/*if (numberInput.value.includes('+')) {
-        numberInput.value.replace(/[+]/g, '');
-        console.log( typeof numberInput.value);
-        console.log(numberInput.value);
-} */
 const numberValue = Number(numberInput.value);
 console.log(numberValue);
-
- 
 
  if (!numberInput.value) {
     
@@ -293,13 +419,6 @@ console.log(numberValue);
  } else {
         
          removeHidden();
-          //result = '';
-
-        /* if (numberInput.value.length === 1) {
-                addOnes(numberValue);
-         } else if (numberInput.value.length === 2) {
-                addTwos(numberInput.value);
-         }    */
 
          displayRomanNumber(numberValue);
          
